187852995c84e75f3f81f42bbcd8108030e5edb5
MostafaAyman77 <mostafaelalf77@gmail.com>
2025-04-12 21:31:49 +0200

    update css and delete Coach screen from home screen

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import OurServices from "../../Components/ServicesSection/OurServices";
 import OurPlans from "../../Components/PlanSection/OurPlans";
 import Heading from "../../Components/Heading/Heading";
-import Coach from "./CoachScreen/Coach";
 import SliderSection from "../../Components/Slider/Slider";
 import AccordionSection from "../../Components/Accordion/AccordionSection";
 
@@ -16,7 +15,6 @@ const Home = () => {
       <OurPlans />
       <SliderSection />
       <AccordionSection />
-      <Coach />
     </div>
   );
 };
