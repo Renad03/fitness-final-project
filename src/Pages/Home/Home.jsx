8121bec3763b979f982aee61d4ea4d9b5d72f085
Renad <renad03emara@gmail.com>
2025-04-15 21:23:33 +0200

    Layout Component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,18 +3,16 @@ import OurServices from "../../Components/ServicesSection/OurServices";
 import OurPlans from "../../Components/PlanSection/OurPlans";
 import SliderSection from "../../Components/Slider/Slider";
 import AccordionSection from "../../Components/Accordion/AccordionSection";
-import Header from "../../Components/Header/Header";
-import Footer from "../../Components/Footer/Footer";
+import Header from "../../Components/Header";
+import Footer from "../../Components/Footer";
 
 const Home = () => {
   return (
     <div id="home">
-      <Header />
       <OurServices />
       <OurPlans />
       <SliderSection />
       <AccordionSection />
-      <Footer />
     </div>
   );
 };
