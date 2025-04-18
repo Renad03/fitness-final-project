7d655b902e7edb1d272f0acaf644a80b46b2c6f7
Renad <renad03emara@gmail.com>
2025-04-18 19:06:31 +0200

    Scroll To Top Problem is Fixed

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import Header from "../Header";
 import { Outlet } from 'react-router-dom';
 import Footer from '../Footer';
+import ScrollToTop from '../ScrollToTop';
 const Layout = () => {
  return <> 
+ <ScrollToTop/>
  <Header/>
  <Outlet/>
  <Footer/>
